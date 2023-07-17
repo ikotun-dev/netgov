04db2232cf44ba9d2be1ddfc75b8fb75b8d7857c
Ikotun Collins <danlogan2003@gmail.com>
2023-07-17 17:14:48 +0100

    virtual environment

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import typer
 import psutil
 
@@ -7,38 +6,26 @@ app = typer.Typer()
 
 @app.command()
 def ports():
-    # Get the list of running processes
-    processes = psutil.process_iter(['pid', 'name'])
+    # Get all network connections
+    connections = psutil.net_connections()
 
-    # Refresh the process information
-    processes = [p.info for p in processes]
-
-    # Filter the processes to only include network-related processes
-    network_processes = [p for p in processes if p.get('name') in ['tcp', 'udp']]
+    # Filter the connections to include only TCP and UDP connections
+    network_connections = [conn for conn in connections if conn.status == psutil.CONN_ESTABLISHED]
 
     # Print the open ports and the corresponding process names
-    for proc in network_processes:
-        connections = proc.connections()
-        for conn in connections:
-            typer.echo(f"Port: {conn.laddr.port} - Process: {proc['name']}")
+    for conn in network_connections:
+        typer.echo(f"Port: {conn.laddr.port} - Process: {psutil.Process(conn.pid).name()}")
 
 
 @app.command()
 def close(port: int):
-    # Iterate over the network processes
-    for proc in psutil.process_iter(['pid', 'name']):
-        proc_info = proc.info
-
-        # Refresh the process information
-        proc_info = proc_info if proc_info else proc.info
-
-        connections = proc_info.connections()
-        for conn in connections:
-            if conn.laddr.port == port:
-                # Terminate the process that is using the specified port
-                proc.kill()
-                typer.echo(f"Port {port} closed successfully.")
-                return
+    # Iterate over all network connections
+    for conn in psutil.net_connections():
+        if conn.status == psutil.CONN_ESTABLISHED and conn.laddr.port == port:
+            # Terminate the process that is using the specified port
+            psutil.Process(conn.pid).kill()
+            typer.echo(f"Port {port} closed successfully.")
+            return
 
     typer.echo(f"No process found using port {port}.")
 
